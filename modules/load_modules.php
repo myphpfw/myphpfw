2257381181b753a24275c2e3284acba585d80c2f
Bryan Joshua Pedini <b.pedini@bjphoster.com>
2020-12-29 10:44:20 +0100

    removed unnecessary redundance

--- modules/load_modules.php
+++ modules/load_modules.php
@@ -1,9 +1,7 @@
 <?php
     declare(strict_types=1);
     require_once(__DIR__."/../modules.php");
-    $loaded_modules = [];
     foreach($load_modules as $module => $required) {
         $required ? require_once(__DIR__."/".$module."/Object.php") : include_once(__DIR__."/".$module."/Object.php");
-        array_push($loaded_modules, $module);
     }
     unset($load_modules);
